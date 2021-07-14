914d2cb2230633714d3de5dd199b513acd571d66
aaronpeet <aaronpeet@icloud.com>
2021-07-14 15:21:54 -0600

    _draw function

--- app/Controllers/JobsController.js
+++ app/Controllers/JobsController.js
@@ -1,8 +1,31 @@
 import { ProxyState } from "../AppState.js";
-import {jobsService} from "../Services/JobsService.js"
+import { carsService } from "../Services/CarsService.js";
+import { jobsService } from "../Services/JobsService.js"
 
-
+function _draw() {
+    let template = ''
+    ProxyState.jobs.forEach(jobs => {
+        template += car.Template
+    })
+    document.getElementById('jobs').innerHTML = template
+}
 
 export default class JobsController{
-    constructor()
+    constructor() {
+        ProxyState.on('jobs', _draw)
+    _draw()
+    }
+
+    createJob() {
+        event.preventDefault()
+        let form = event.target
+        let rawJob = {
+            company: form.company.value,
+            position: form.position.value,
+            salary: form.salary.value,
+            logo: form.logo.value,
+            description: form.description.value
+        }
+}
+
 }
